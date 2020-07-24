906140710404243a515d6a6dad5567218eee53a4
Sanaa514 <sanaamontgomery10@gmail.com>
2020-07-24 15:06:44 +0000

    part 2 was added

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ for i in range(6):
 
   for j in range(6):
     print("inner loop | i = " + str(i) + " | j = " + str(j))
+
+
+numbers = range(0,10)
+
+for i in range(10):
+  print("outer loop | i = " + str(i))
+
+  for j in range(10):
+    print("inner loop | i = " + str(i) + " | j = " + str(j))
